c1cce4a005ded08333394f43d54f0eb245ef4bee
Yanik Proulx <yproulx@perimetre.co>
2018-02-06 13:32:46 -0500

    single blog post

---
+++ wp-content/themes/fondationmolson/single-post.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The Template for displaying a single post
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::get_context();
+$post = Timber::query_post();
+$context['post'] = $post;
+
+Timber::render('single-post.twig', $context );
