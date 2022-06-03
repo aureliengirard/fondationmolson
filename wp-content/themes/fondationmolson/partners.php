839bffb6c039ae8e71dcf07535ada05ceee648fb
Aurelien Girard <aurelien.girard@gmail.com>
2022-06-03 16:07:48 -0400

    new template for grant table

--- wp-content/themes/fondationmolson/partners.php
+++ wp-content/themes/fondationmolson/partners.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Template Name: Fields of Intervention Page Template
+* Template Name: Partners Page Template
  *
  * @package  WordPress
  * @subpackage  Timber
@@ -11,4 +11,4 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['page'] = $post;
 
-Timber::render('fields_of_intervention.twig', $context);
+Timber::render('partners.twig', $context);
